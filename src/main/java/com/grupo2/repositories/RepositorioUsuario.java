a536e595902e8328455e53a959cf2c36b9a9d6ab
RusukeHidashi <rusuke_@hotmail.com>
2025-06-02 14:58:16 +0200

    areglados

---
+++ src/main/java/com/grupo2/repositories/RepositorioUsuario.java
@@ -0,0 +1,10 @@
+package com.grupo2.repositories;
+
+import com.grupo2.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    // Consultas personalizadas si es necesario
+}
