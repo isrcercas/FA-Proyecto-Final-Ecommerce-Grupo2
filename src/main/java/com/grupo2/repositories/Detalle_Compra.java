b1dbbaf5ab30fe037c76f66f4787cad6c68d0368
RusukeHidashi <rusuke_@hotmail.com>
2025-06-02 13:56:18 +0200

    añadido repositorios

---
+++ src/main/java/com/grupo2/repositories/Detalle_Compra.java
@@ -0,0 +1,14 @@
+package com.grupo2.repositories;
+
+public class Detalle_Compra {
+    package com.grupo2.repositories;
+
+import com.grupo2.entities.Detalle_Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    @Repository
+    public interface DetalleCompraRepository extends JpaRepository<Detalle_Compra, Long> {
+        // Consultas personalizadas si es necesario
+    }
+}
