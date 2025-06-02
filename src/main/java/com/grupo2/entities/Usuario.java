fe0e6e0cbcd2f895e7cba1374db8e71c7e458dbb
AntonioMoraLopez <antoniomora.estudio@gmail.com>
2025-06-02 12:35:32 +0200

    Entidades hechas

--- src/main/java/com/grupo2/entities/Usuario.java
+++ src/main/java/com/grupo2/entities/Usuario.java
@@ -1,4 +1,82 @@
 package com.grupo2.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table (name = "usuarios")
 public class Usuario {
+@Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+@Column (nullable = false)
+    private String nombreUsuario;
+    private String password;
+    private String email;
+    private String direccion;
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Compra> compras = new ArrayList<>();
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, String password, String email, String direccion) {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombreUsuario='" + nombreUsuario + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
 }
