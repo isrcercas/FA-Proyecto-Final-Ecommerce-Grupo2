8fa329c48fcf6bbb87c81f1a618816cdfc077f8b
vruss <tucusitovr@gmail.com>
2025-06-06 10:45:04 +0200

    CategoriaController categoria-list.html categoria-detail.html categoria-form.html

--- src/main/java/com/grupo2/controllers/CategoriaController.java
+++ src/main/java/com/grupo2/controllers/CategoriaController.java
@@ -2,16 +2,24 @@ package com.grupo2.controllers;
 
 import com.grupo2.entities.Categoria;
 import com.grupo2.repositories.CategoriaRepository;
+import com.grupo2.repositories.ProductoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.grupo2.repositories.ProductoRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Controller
 public class CategoriaController {
 
+    private ProductoRepository productoRepository;
     private CategoriaRepository categoriaRepository;
+
+
 
     public CategoriaController(CategoriaRepository categoriaRepository){
         this.categoriaRepository = categoriaRepository;
@@ -22,6 +30,54 @@ public class CategoriaController {
 
         List<Categoria> categorias = categoriaRepository.findAll();
         model.addAttribute("categorias", categorias);
+
+        return "categoria/categoria-list";
+    }
+
+
+    @GetMapping("/categorias/nueva")
+    public String createForm(Model model) {
+        model.addAttribute("categoria", new Categoria());
+
+        return "categoria/categoria-form";
+    }
+
+    @GetMapping("/categorias/{id}/editar")
+    public String editForm(Model model, @PathVariable Long id) {
+        Optional<Categoria> categoriaOpt = categoriaRepository.findById(id);
+
+        if (categoriaOpt.isPresent()) {
+            model.addAttribute("categoria", categoriaOpt.get());
+        } else {
+            model.addAttribute("error", "404 Categoría Not Found");
+        }
+
+        return "categoria/categoria-form";
+    }
+
+    @PostMapping("/categorias")
+    public String saveForm(@ModelAttribute Categoria categoria) {
+        categoriaRepository.save(categoria);
+
+        return "redirect:/categorias";
+    }
+
+    @PostMapping("/categorias/{id}/eliminar")
+    public String delete(@PathVariable Long id) {
+        if (productoRepository.countByCategoria_Id(id) > 0) {
+            return "redirect:/categorias?error=true";
+        }
+        categoriaRepository.deleteById(id);
+
+        return "redirect:/categorias";
+    }
+
+    // filtrar categorías por nombre
+    @GetMapping("/categorias/buscar") // http:localhost:8080/categorias/buscar?nombre=frutas
+    public String findByName(Model model, @RequestParam("nombre") String nombre) {
+        List<Categoria> categorias = categoriaRepository.findByNombreContainsIgnoreCase(nombre);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("busquedaActual", nombre);
 
         return "categoria/categoria-list";
     }
