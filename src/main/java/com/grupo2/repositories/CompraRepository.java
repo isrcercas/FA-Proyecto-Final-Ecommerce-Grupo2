42bfb47675da675fc72c5086c8eff2ce0bf7e1ec
isrcercas <isrcercas@gmail.com>
2025-06-25 17:59:10 +0200

    Corregido cambio fecha fija y oculta

--- src/main/java/com/grupo2/repositories/CompraRepository.java
+++ src/main/java/com/grupo2/repositories/CompraRepository.java
@@ -2,20 +2,11 @@ package com.grupo2.repositories;
 
 import com.grupo2.entities.Compra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompraRepository extends JpaRepository<Compra, Long> {
+    @Query("select sum(c) from Compra c where c.total = ?1")
+    long sumByTotal(Double total);
 
 
-
-    /*
-
-    findAllByUserId para traer todas las compras
-
-    form
-    detalle compra
-    select productos
-    select compras
-    select
-
-     */
 }
